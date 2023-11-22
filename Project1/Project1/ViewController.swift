b16b39e9d5fe667dd4708daa6b35eb8d60e3b0f1
baifumei <k_katerina_k@icloud.com>
2023-11-22 16:41:58 +0300

    Listing images with FileManager

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //to interact with the file system
+        let fm = FileManager.default
+        
+        //a path to the resource directory of the main application bundle
+        let path = Bundle.main.resourcePath!
+        
+        //an array of strings representing the names of files and folders located at the specified path
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
-
-
 }
 
