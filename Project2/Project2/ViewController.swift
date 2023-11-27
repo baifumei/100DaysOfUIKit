25e643def7e92e038dfd0d6291b92f09c304fa81
baifumei <k_katerina_k@icloud.com>
2023-11-27 22:30:17 +0300

    Add a border around the flags buttons

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
 
         askQuestion()
     }
