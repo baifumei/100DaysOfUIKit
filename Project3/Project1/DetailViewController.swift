2f819f9f97e733e90e2e60026b851b40b566a225
baifumei <k_katerina_k@icloud.com>
2023-12-05 12:12:55 +0300

    Create project3 from a copy of project1

---
+++ Project3/Project1/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Екатерина К on 23.11.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var indexNumber: Int = 0
+    var numberOfPictures: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(indexNumber) of \(numberOfPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
