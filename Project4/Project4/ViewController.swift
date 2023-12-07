586626916fce46aaf7213ae27ab18035a57e9a86
baifumei <k_katerina_k@icloud.com>
2023-12-07 17:31:49 +0300

    Create a simple browser with WKWebView

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com/")!
+        webView.load(URLRequest(url: url))
+        //enables a property on the web view that allows users to swipe from the left or right edge to move backward or forward in their web browsing
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
 
 
