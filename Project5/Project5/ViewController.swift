5f36b40f185164e2456410a76ab216a36149cd74
baifumei <k_katerina_k@icloud.com>
2023-12-14 16:42:25 +0300

    Create a func promptForAnswer

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //add + in rightBarButtonItem
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         //finding the path to start.txt file
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             //loading the contents of that file
@@ -44,5 +47,21 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String ) {
+        
     }
 }
