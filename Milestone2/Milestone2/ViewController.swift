c9847df08fb5f9b59ddbaff932b410fab98d597d
baifumei <k_katerina_k@icloud.com>
2023-12-19 10:59:58 +0300

    Create a new project for Milestone2

---
+++ Milestone2/Milestone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone2
+//
+//  Created by Екатерина К on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
