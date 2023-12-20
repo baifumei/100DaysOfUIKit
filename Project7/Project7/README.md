61896e71f0f49f4c5f541e76ae4e46dcc66cefb0
baifumei <k_katerina_k@icloud.com>
2023-12-20 14:59:43 +0300

    Solve chalenge2

--- Project7/Project7/README.md
+++ Project7/Project7/README.md
@@ -15,8 +15,8 @@
 
 ## Challenge
 
-- [] Add a Credits button to the top-right corner using UIBarButtonItem. When this is tapped, show an alert telling users the data comes from the We The People API of the Whitehouse.
+- [x] Add a Credits button to the top-right corner using UIBarButtonItem. When this is tapped, show an alert telling users the data comes from the We The People API of the Whitehouse.
 - [] Let users filter the petitions they see. This involves creating a second array of filtered items that contains only petitions matching a string the user entered. Use a UIAlertController with a text field to let them enter that string. This is a tough one, so I’ve included some hints below if you get stuck.
 - [] Experiment with the HTML – this isn’t a HTML or CSS tutorial, but you can find lots of resources online to give you enough knowledge to tinker with the layout a little.
 
-<img width="300" alt="P2" src="https://sun9-69.userapi.com/impg/xvmkgQo39M8ctEh8q5Q5B29J0pZ8Yz82jT2rBw/BTJuG1h7rhI.jpg?size=640x1338&quality=96&sign=7f1057b133e1d5b040729c5d16af9c54&type=album">
+<img width="300" alt="P2" src="https://sun9-69.userapi.com/impg/xvmkgQo39M8ctEh8q5Q5B29J0pZ8Yz82jT2rBw/BTJuG1h7rhI.jpg?size=640x1338&quality=96&sign=7f1057b133e1d5b040729c5d16af9c54&type=album"> <img width="300" alt="P2" src="">
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var findingWords = ""
+    var filtedPetitions: [Petition] {
+        if findingWords.isEmpty {
+            return petitions
+        } else {
+            return petitions.filter {
+                $0.title.contains(findingWords)
+            }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let urlString: String
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "doc"), style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearch))
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -39,6 +51,22 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    
+    //MARK: - Search
+    @objc func showSearch() {
+        let ac = UIAlertController(title: "Search in petitions", message: "Enter text, that must be in the title of a petition", preferredStyle: .alert)
+        ac.addTextField()
+        let action = UIAlertAction(title: "Continue", style: .cancel) { [weak self, weak ac] _ in
+            //
+            guard let search = ac?.textFields?[0].text else { return }
+            self?.findingWords = search
+            self?.tableView.reloadData()
+        }
+        ac.addAction(action)
+        present(ac, animated: true)
+    }
+    
+    
     //MARK: - Credits
     @objc func showCredits() {
         let ac = UIAlertController(title: "Source", message: "Data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
@@ -60,12 +88,12 @@ class ViewController: UITableViewController {
     
     //MARK: - tableView
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filtedPetitions.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        let petition = petitions[indexPath.row]
+        let petition = filtedPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         
@@ -73,7 +101,7 @@ class ViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let ac = DetailViewController()
-        ac.detailItem = petitions[indexPath.row]
+        ac.detailItem = filtedPetitions[indexPath.row]
         navigationController?.pushViewController(ac, animated: true)
     }
 }
