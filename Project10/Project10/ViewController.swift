46c8eb33c338216f2d4455bd5b7fedcb7c358996
baifumei <k_katerina_k@icloud.com>
2023-12-29 16:06:33 +0300

    Create a project10 file

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Екатерина К on 29.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
