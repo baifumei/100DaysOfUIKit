0088752125cc6937a3a8d623a8cd0716692bf64f
baifumei <k_katerina_k@icloud.com>
2024-01-17 21:57:55 +0300

    Create a new project11 and add assets

---
+++ Project11/Project11/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Екатерина К on 17.01.2024.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
