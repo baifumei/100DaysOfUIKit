c3e6800df769554e2444423f2912b658bf4ac33a
baifumei <k_katerina_k@icloud.com>
2024-01-23 18:32:16 +0300

    Create a new project12 about UserDefaults

---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Екатерина К on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = UserDefaults.standard
+
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseTouchID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("George May Atwood", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDict")
+        
+        let SavedArray = defaults.object(forKey:"SavedArray") as? [String] ?? [String]()
+        let SavedDict = defaults.object(forKey: "SavedDict") as? [String: String] ?? [String: String]()
+
+    }
+
+
+}
+
