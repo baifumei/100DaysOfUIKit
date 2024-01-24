715cd23471e2228ba42a27fb2cd7bcda55322c1a
baifumei <k_katerina_k@icloud.com>
2024-01-24 17:13:43 +0300

    Modify Person class

--- Project12a/Project10/Person.swift
+++ Project12a/Project10/Person.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
+    //запись на диск
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    //чтение с диска
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    
     var name: String
     var image: String
     
