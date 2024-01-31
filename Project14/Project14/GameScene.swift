5b6f879b5ef89ea84f6ca2243b472404fe933872
baifumei <k_katerina_k@icloud.com>
2024-01-31 14:23:13 +0300

    Add background to the screen

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -8,9 +8,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
