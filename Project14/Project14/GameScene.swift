6f7768fa3626167f434811ea8f670ed5b7dd35d7
baifumei <k_katerina_k@icloud.com>
2024-01-31 13:27:26 +0300

    Create a new Project14 file

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Екатерина К on 1/31/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
