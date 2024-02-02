0937cd1420d6e54a50e5ef4576984a7e88b97684
baifumei <k_katerina_k@icloud.com>
2024-02-02 19:57:19 +0300

    Prepare for action

---
+++ Project15/Project15/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Екатерина К on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+    }
+
+    @IBAction func tapped(_ sender: Any) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+}
+
