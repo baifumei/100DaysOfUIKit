54e5af8182d07465fff906391899c950d977f98e
baifumei <k_katerina_k@icloud.com>
2024-02-07 18:50:46 +0300

    Create a new project16

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Екатерина К on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
