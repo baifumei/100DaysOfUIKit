9f0c00523023a16267049c4f763d65419d0e8eec
baifumei <k_katerina_k@icloud.com>
2024-02-08 15:50:11 +0300

    Add capitals to the map

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Екатерина К on 2/8/24.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.info = info
+    }
+
+}
