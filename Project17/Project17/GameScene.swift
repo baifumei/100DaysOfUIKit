dabc284aff8f1dc53813761df3bc368e48b89fa7
baifumei <k_katerina_k@icloud.com>
2024-02-12 16:05:07 +0300

    Create a new project17

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Екатерина К on 2/12/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+
+    }
+}
