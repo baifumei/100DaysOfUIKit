839e9ce7798aafff4b9ed7a99a41c166b164fd22
baifumei <k_katerina_k@icloud.com>
2024-02-12 23:28:45 +0300

    Create a space, rocket and score

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -7,11 +7,41 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLable: SKLabelNode!
     
+    var score = 0 {
+        didSet {
+            scoreLable.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        backgroundColor = .black
         
+        starfield = SKEmitterNode(fileNamed: "starfield")
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        starfield.zPosition = -1
+        addChild(starfield)
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLable = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLable.position = CGPoint(x: 16, y: 16)
+        scoreLable.horizontalAlignmentMode = .left
+        addChild(scoreLable)
+        
+        score = 0
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
     }
     
     override func update(_ currentTime: TimeInterval) {
