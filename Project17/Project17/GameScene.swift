01c4427f38becd50ef8a84b1b9d5086d70c71de3
baifumei <k_katerina_k@icloud.com>
2024-02-14 14:33:17 +0300

    Challenge1 done

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -20,6 +20,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var possibleEnemies = ["ball", "hammer", "tv"]
     var gameTimer: Timer?
+    var timeInterval: Double = 1
+    var enemiesCount = 0
     
     var isGameOver = false {
         didSet {
@@ -61,20 +63,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     @objc func createEnemy() {
+        if isGameOver { return }
         guard let enemy = possibleEnemies.randomElement() else { return }
         
-        if !isGameOver {
-            let sprite = SKSpriteNode(imageNamed: enemy)
-            sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
-            sprite.zPosition = 1
-            addChild(sprite)
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        sprite.zPosition = 1
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+        enemiesCount += 1
+        
+        if enemiesCount == 20 {
+            enemiesCount = 0
+            gameTimer?.invalidate()
+            timeInterval -= 0.1
+            gameTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
             
-            sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
-            sprite.physicsBody?.categoryBitMask = 1
-            sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
-            sprite.physicsBody?.angularVelocity = 5
-            sprite.physicsBody?.linearDamping = 0
-            sprite.physicsBody?.angularDamping = 0
         }
     }
     
