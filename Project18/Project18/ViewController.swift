9b492136b4ef98c5da7dac90ae77641565bfefbc
baifumei <k_katerina_k@icloud.com>
2024-02-14 16:45:25 +0300

    Add first debugging technique - print()

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Екатерина К on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //print()
+        print("I'm inside the viewDidLoad() method!")
+        print(1, 2, 3, 4, 5, terminator: "")
+        print(1, 2, 3, 4, 5, separator: "-")
+        
+    }
+}
+
