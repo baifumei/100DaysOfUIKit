2263ac66d2cd8b68449e08f37cea636aaa320e5b
baifumei <k_katerina_k@icloud.com>
2024-02-15 14:29:55 +0300

    Create a new file action estention

---
+++ Project19/Extention/ActionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ActionViewController.swift
+//  Extention
+//
+//  Created by Екатерина К on 2/15/24.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
