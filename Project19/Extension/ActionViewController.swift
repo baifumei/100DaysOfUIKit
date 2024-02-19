3d45d1299a7d1605a77ed5861191f9760fbf336f
baifumei <k_katerina_k@icloud.com>
2024-02-19 14:37:16 +0300

    Reorganize the main interface

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -11,8 +11,11 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -21,7 +24,14 @@ class ActionViewController: UIViewController {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
                     guard let itemDictionary = dict as? NSDictionary else { return }
                     guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-                    print(javaScriptValues)
+                    
+                    
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
                 }
             }
         }
