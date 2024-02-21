bee186be804636e60cfdf77b2cf869eff8a1e9ee
baifumei <k_katerina_k@icloud.com>
2024-02-21 16:42:33 +0300

    Add background, create fireworks and timer

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -8,8 +8,49 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+    
+    var leftEdge = -22
+    var bottomEdge = -22
+    var rightEdge = 1024 + 33
+    
+    var score: Int = 0 {
+        didSet {
+            
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    @objc func launchFireworks() {
+        
+    }
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        firework.zPosition = 1
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .red
+        default:
+            firework.color = .green
+        }
     }
 }
