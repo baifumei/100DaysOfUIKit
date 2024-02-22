5a6ea684ad8c66d3cbb0021741fed24c2e7891d3
baifumei <k_katerina_k@icloud.com>
2024-02-22 16:38:49 +0300

    Create explode function

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -144,4 +144,40 @@ class GameScene: SKScene {
             }
         }
     }
+    
+    func explode(firework: SKNode) {
+        if let emitter = SKEmitterNode(fileNamed: "explode") {
+            emitter.position = firework.position
+            addChild(emitter)
+        }
+        firework.removeFromParent()
+    }
+    
+    func explodeFirewirks() {
+        var numbExplode = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            
+            if firework.name == "selected" {
+                explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numbExplode += 1
+            }
+        }
+        switch numbExplode {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
+        }
+    }
 }
