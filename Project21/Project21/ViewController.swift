778bf4651ec5ba5ee21a2a7191bb1a7fdd95c740
baifumei <k_katerina_k@icloud.com>
2024-03-04 17:21:03 +0900

    Request permission

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+
     }
 
-
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("yes")
+            } else {
+                print("no")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
 }
 
