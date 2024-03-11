07a25771b53fcedc297274c3a082f297e5adfa27
baifumei <k_katerina_k@icloud.com>
2024-03-11 17:46:02 +0300

    Create a new project and add CLLocationManager

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Екатерина К on 3/11/24.
+//
+
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+
+
+}
+
