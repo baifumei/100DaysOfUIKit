0daf6d6b0cf26be0c2594d85fc5a75bd13dc11f9
baifumei <k_katerina_k@icloud.com>
2024-04-08 22:43:13 -0700

    Going peer to peer: MCSession, MCBrowserViewController

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -5,19 +5,47 @@
 //  Created by Екатерина К on 4/8/24.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MCBrowserViewControllerDelegate, MCSessionDelegate {
     
     var images = [UIImage]()
+    
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Selfie share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importImage))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
     }
+    /*
+     Multipeer connectivity requires four new classes:
+
+     1) MCSession is the manager class that handles all multipeer connectivity for us.
+     2) MCPeerID identifies each user uniquely in a session.
+     3) MCAdvertiserAssistant is used when creating a session, telling others that we exist and handling invitations.
+     4) MCBrowserViewController is used when looking for sessions, showing users who is nearby and letting them join.
+    */
+        
+        func startHosting(action: UIAlertAction) {
+            guard let mcSession = mcSession else { return }
+            mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        }
+        
+        func joinSession(action: UIAlertAction) {
+            guard let mcSession = mcSession else { return }
+            let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+            mcBrowser.delegate = self
+            present(mcBrowser, animated: true)
+        }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
@@ -45,6 +73,14 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         images.insert(image, at: 0)
         collectionView.reloadData()
+    }
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
 }
 
