274301f1385068f778272a21df0019fe11945cbb
baifumei <k_katerina_k@icloud.com>
2024-04-10 00:17:22 -0700

    Move the player by using touchesBegan/Moved/Ended

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -18,6 +18,9 @@ enum CollisionType: UInt32 {
 class GameScene: SKScene {
     var player: SKSpriteNode!
     
+    //a hack that lets us simulate the experience of moving the ball using touch
+    var lastTouchPosition: CGPoint?
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -27,6 +30,8 @@ class GameScene: SKScene {
         
         loadLevel()
         createPlayer()
+        
+        physicsWorld.gravity = .zero
     }
     
     func loadLevel() {
@@ -119,5 +124,28 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = CollisionType.wall.rawValue
         
         addChild(player)
+    }
+     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let lastTouchPosition = lastTouchPosition {
+            let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
     }
 }
