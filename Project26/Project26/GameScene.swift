44094c6889432039d62e045d2d579429eada48ca
baifumei <k_katerina_k@icloud.com>
2024-04-09 21:51:34 -0700

    Set assets to the screen and their bitmask

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -7,9 +7,17 @@
 
 import SpriteKit
 
+enum CollisionType: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-        
+        loadLevel()
     }
     
     func loadLevel() {
@@ -20,21 +28,68 @@ class GameScene: SKScene {
             fatalError("Could not load level1.txt file from app bundle.")
         }
         
-        let lines = levelString.components(separatedBy: "/n")
+        let lines = levelString.components(separatedBy: "\n")
         
         for (row, line) in lines.reversed().enumerated() {
             for (column, letter) in line.enumerated() {
                 let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
                 if letter == "x" {
                     //block
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionType.wall.rawValue //2
+                    node.physicsBody?.isDynamic = false
+                    
+                    addChild(node)
+                    
                 } else if letter == "s" {
-                    //star
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.name = "star"
+                    node.position = position
+                    
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    
+                    node.physicsBody?.categoryBitMask = CollisionType.star.rawValue // 4
+                    node.physicsBody?.contactTestBitMask = CollisionType.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    
+                    addChild(node)
+                    
                 } else if letter == "v" {
-                    //vortex
+                    let node = SKSpriteNode(imageNamed: "vortex")
+                    node.name = "vortex"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    
+                    node.physicsBody?.categoryBitMask = CollisionType.vortex.rawValue // 8
+                    node.physicsBody?.contactTestBitMask = CollisionType.player.rawValue // 1
+                    node.physicsBody?.collisionBitMask = 0
+                    
+                    addChild(node)
+                    
                 } else if letter == "f" {
-                    //finish
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.name = "finish"
+                    node.position = position
+                    
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionType.finish.rawValue // 16
+                    node.physicsBody?.contactTestBitMask = CollisionType.player.rawValue // 1
+                    node.physicsBody?.collisionBitMask = 0
+
+                    addChild(node)
+                    
                 } else if letter == " " {
-                    // для пробела
+                    //this is an empty space - do nothing
                 } else {
                     fatalError("Unlnown letter: \(letter)")
                 }
