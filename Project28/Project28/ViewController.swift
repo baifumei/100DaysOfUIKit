55d81e617e09dbb466d02e5edc6b8b4eeb632b58
baifumei <k_katerina_k@icloud.com>
2024-04-16 19:39:39 -0700

    Finish - Writing somewhere safe: the iOS keychain

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -12,18 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet var secret: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Nothing to see here!"
     
         let notification = NotificationCenter.default
         notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-        
+        notification.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
+        unlockSecretMessage()
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
-        
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         
         let keyboardScreenEnd = keyboardValue.cgRectValue
@@ -31,15 +33,30 @@ class ViewController: UIViewController {
         
         if notification.name == UIResponder.keyboardWillHideNotification {
             secret.contentInset = .zero
-                } else {
-                    secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
-                }
-
-                secret.scrollIndicatorInsets = secret.contentInset
-
-                let selectedRange = secret.selectedRange
-                secret.scrollRangeToVisible(selectedRange)
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
         }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+    
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here!"
     }
 }
 
