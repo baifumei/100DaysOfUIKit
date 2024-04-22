3ea2b37f746316cd831721cc4b150bc14ff6e915
baifumei <k_katerina_k@icloud.com>
2024-04-22 01:50:18 -0700

    Create a new project29

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Екатерина К on 4/22/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+        
+    override func didMove(to view: SKView) {
+        
+    }
+}
